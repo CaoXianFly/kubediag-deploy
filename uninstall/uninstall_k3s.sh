14114dff395dfb442f15d19a15b5961025ce5659
Cao Xianfei <18251716983@163.com>
2022-11-25 10:01:38 +0800

    use containerd instead docker

--- uninstall/uninstall_k3s.sh
+++ uninstall/uninstall_k3s.sh
@@ -2,9 +2,17 @@
 rm -rf ~/.kube/*
 sh /usr/local/bin/k3s-uninstall.sh
 
-apt-get remove docker docker-engine docker.io containerd runc -y
+# Uninstall docker on Ubuntu
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 
-yum remove -y docker \
+sudo apt-get remove docker docker-ce docker-ce-* docker-engine docker.io containerd runc -y
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+
+# Uninstall docker on CentOS
+sudo yum remove -y docker \
                 docker-client \
                 docker-client-latest \
                 docker-common \
@@ -14,3 +22,8 @@ yum remove -y docker \
                 docker-engine \
                 podman \
                 runc
+
+sudo yum remove -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
